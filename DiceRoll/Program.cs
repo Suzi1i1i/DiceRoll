17b541f3fe67899e0c39572b37641ea828cb8074
suzie <puppychico0930@gmail.com>
2024-01-31 23:25:27 -0700

    -

--- DiceRoll/Program.cs
+++ DiceRoll/Program.cs
@@ -1,19 +1,33 @@
 ﻿using DiceRoll;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 static void DiceRoll()
 {
+    DiceCalc dc = new DiceCalc();
     int input = 0;
 
     System.Console.WriteLine("Welcome to the dice throwing simulator!");
     System.Console.WriteLine("How many dice rolls would you like to simulate?");
-    input = System.Console.ReadLine();
+    input = Convert.ToInt32(Console.ReadLine());
 
-    for (int i = 0; i < 12; i++)
+    int[] Results = dc.DiceNumCalc(input);
+
+    Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+    Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+    Console.WriteLine("Total number of rolls =" + input + "\n");
+    for (int i = 0; i < 11; i++)
     {
+        Console.Write((i + 2) + ": ");
+        int percentage = (int)((Results[i] / (double)input) * 100);
 
-        myArray[i] / input * 100 = 
+        for (int j = 0; j < percentage; j++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
     }
 
-    Console.WriteLine("DICE ROLLING SIMULATION RESULTS\r\nEach \"*\" represents 1% of the total number of rolls.\r\nTotal number of rolls = 1000.");
 
+    Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
 }
+
